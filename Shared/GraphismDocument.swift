05ea154df238ddbddd3c92c6b40b1393ce38eb9e
Snowy <3mille.prenom.nom@gmail.com>
2020-06-30 17:52:38 +0200

    GRPH predefined colors now directly in PaintColor instead of wrapped

--- Shared/GraphismDocument.swift
+++ Shared/GraphismDocument.swift
@@ -22,16 +22,16 @@ struct GraphismDocument: FileDocument {
         if let shapes = shapes {
             self.shapes = shapes
         } else {
-            self.shapes = [GRectangle(position: Pos(x: 200, y: 100), size: Pos(x: 150, y: 100), paint: .color(.wrapping(.red))),
-                           GRectangle(position: Pos(x: 40, y: 0), size: Pos(x: 50, y: 100), paint: .color(.wrapping(.green)), strokeStyle: StrokeWrapper(strokeWidth: 5, strokeType: .rounded, strokeDashArray: GRPHArray([1, 10], of: SimpleType.float))),
-                           GRectangle(position: Pos(x: 250, y: 125), size: Pos(x: 50, y: 50), rotation: 45, paint: .color(.wrapping(.yellow))),
-                           GCircle(position: Pos(x: 260, y: 135), size: Pos(x: 30, y: 30), paint: .color(.wrapping(.red))),
-                           GCircle(position: Pos(x: 10, y: 200), size: Pos(x: 200, y: 150), paint: .linear(.init(from: .wrapping(.blue), direction: .right, to: .wrapping(.green)))),
-                           GLine(start: Pos(x: 275, y: 150), end: Pos(x: 110, y: 275), paint: .color(.wrapping(.black)), strokeStyle: StrokeWrapper(strokeType: .rounded)),
-                           GPath(givenName: "heart", points: [Pos(x: 150, y: 210), Pos(x: 75, y: 165), Pos(x: 50, y: 55), Pos(x: 150, y: 110), Pos(x: 250, y: 55), Pos(x: 225, y: 165), Pos(x: 150, y: 210)], actions: [.moveTo, .cubicTo, .cubicTo], paint: .color(.wrapping(.purple))),
-                           GClip(shape: GRectangle(position: Pos(x: 300, y: 300), size: Pos(x: 50, y: 50), paint: .radial(.init(centerColor: .wrapping(.orange), externalColor: .wrapping(.red), radius: 25))),
-                                 clip: GRectangle(position: Pos(x: 300, y: 300), size: Pos(x: 50, y: 50), rotation: 45, paint: .color(.wrapping(.black)))),
-                           GPolygon(points: [Pos(x: 200, y: 0), Pos(x: 300, y: 0), Pos(x: 275, y: 30), Pos(x: 300, y: 60), Pos(x: 200, y: 60), Pos(x: 225, y: 30)], paint: .color(.wrapping(.orange)))]
+            self.shapes = [GRectangle(position: Pos(x: 200, y: 100), size: Pos(x: 150, y: 100), paint: .color(.red)),
+                           GRectangle(position: Pos(x: 40, y: 0), size: Pos(x: 50, y: 100), paint: .color(.green), strokeStyle: StrokeWrapper(strokeWidth: 5, strokeType: .rounded, strokeDashArray: GRPHArray([1, 10], of: SimpleType.float))),
+                           GRectangle(position: Pos(x: 250, y: 125), size: Pos(x: 50, y: 50), rotation: 45, paint: .color(.yellow)),
+                           GCircle(position: Pos(x: 260, y: 135), size: Pos(x: 30, y: 30), paint: .color(.red)),
+                           GCircle(position: Pos(x: 10, y: 200), size: Pos(x: 200, y: 150), paint: .linear(.init(from: .blue, direction: .right, to: .green))),
+                           GLine(start: Pos(x: 275, y: 150), end: Pos(x: 110, y: 275), paint: .color(.black), strokeStyle: StrokeWrapper(strokeType: .rounded)),
+                           GPath(givenName: "heart", points: [Pos(x: 150, y: 210), Pos(x: 75, y: 165), Pos(x: 50, y: 55), Pos(x: 150, y: 110), Pos(x: 250, y: 55), Pos(x: 225, y: 165), Pos(x: 150, y: 210)], actions: [.moveTo, .cubicTo, .cubicTo], paint: .color(.purple)),
+                           GClip(shape: GRectangle(position: Pos(x: 300, y: 300), size: Pos(x: 50, y: 50), paint: .radial(.init(centerColor: .orange, externalColor: .red, radius: 25))),
+                                 clip: GRectangle(position: Pos(x: 300, y: 300), size: Pos(x: 50, y: 50), rotation: 45, paint: .color(.black))),
+                           GPolygon(points: [Pos(x: 200, y: 0), Pos(x: 300, y: 0), Pos(x: 275, y: 30), Pos(x: 300, y: 60), Pos(x: 200, y: 60), Pos(x: 225, y: 30)], paint: .color(.orange))]
         }
         var source = ""
         
--- Shared/Types/ColorPaint.swift
+++ Shared/Types/ColorPaint.swift
@@ -9,25 +9,65 @@ import Foundation
 import SwiftUI
 
 public enum ColorPaint: Paint {
-    case wrapping(Color)
+    case white, black, red, green, blue, orange, yellow, pink, purple, aqua, alpha
     
     case components(red: Float, green: Float, blue: Float, alpha: Float = 1)
     
     public var style: Color {
         switch self {
-        case .wrapping(let color):
-            return color
         case .components(red: let red, green: let green, blue: let blue, alpha: let alpha):
             return Color(red: Double(red), green: Double(green), blue: Double(blue), opacity: Double(alpha))
+        case .white:
+            return .white
+        case .black:
+            return .black
+        case .red:
+            return .red
+        case .green:
+            return .green
+        case .blue:
+            return .blue
+        case .orange:
+            return .orange
+        case .yellow:
+            return .yellow
+        case .pink:
+            return .pink
+        case .purple:
+            return .purple
+        case .aqua:
+            return .init(red: 0, green: 0.85, blue: 0.85)
+        case .alpha:
+            return .clear
         }
     }
     
     public var state: String {
         switch self {
-        case .wrapping(let color):
-            return color.description.uppercased()
         case .components(red: let red, green: let green, blue: let blue, alpha: let alpha):
             return "color(\(red) \(green) \(blue) \(alpha))"
+        case .white:
+            return "WHITE"
+        case .black:
+            return "BLACK"
+        case .red:
+            return "RED"
+        case .green:
+            return "GREEN"
+        case .blue:
+            return "BLUE"
+        case .orange:
+            return "ORANGE"
+        case .yellow:
+            return "YELLOW"
+        case .pink:
+            return "PINK"
+        case .purple:
+            return "PURPLE"
+        case .aqua:
+            return "AQUA"
+        case .alpha:
+            return "ALPHA"
         }
     }
     
