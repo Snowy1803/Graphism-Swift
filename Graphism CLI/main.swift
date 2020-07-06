125db34b4069808350ef7df33b5971ec17164708
Snowy <3mille.prenom.nom@gmail.com>
2020-07-06 22:06:35 +0200

    Optimized constructors. Included test is now 3 times faster.

--- Graphism CLI/main.swift
+++ Graphism CLI/main.swift
@@ -10,12 +10,15 @@ import Foundation
 func main() {
     let compiler = GRPHCompiler(entireContent: """
 int i = 0
-#while i < 1000000
-\t#if i * 100000 == 0
-\t\t//log["Iteration" i]
+#while i < 2000000
+\tCircle c = Circle(50,50 pos([10f * i] [10f * i]) color.RED)
+\t#if i % 10000 == 0
+\t\tlog["Iteration" i c]
 \ti += 1
 """)
-    _ = compiler.compile()
+    guard compiler.compile() else {
+        return
+    }
     print(compiler.wdiuInstructions)
     let runtime = GRPHRuntime(compiler: compiler)
     _ = runtime.run()
--- Shared/Expressions/ConstructorExpression.swift
+++ Shared/Expressions/ConstructorExpression.swift
@@ -10,39 +10,30 @@ import Foundation
 struct ConstructorExpression: Expression {
     static let pattern = try! NSRegularExpression(pattern: "^(\(Expressions.typePattern))?\\((.+)\\)$")
     var constructor: Constructor
-    var values: [Expression]
+    var values: [Expression?]
     
     init(ctx: GRPHContext, type: GRPHType, values: [Expression]) throws {
-        self.values = values
         guard let constructor = type.constructor else {
             throw GRPHCompileError(type: .typeMismatch, message: "No constructor found in '\(type)'");
         }
         self.constructor = constructor
         // Java did kinda support multiple constructor but they didn't exist
         var nextParam = 0
+        self.values = []
         for param in values {
             guard let par = try constructor.parameter(index: nextParam, context: ctx, exp: param) else {
                 throw GRPHCompileError(type: .typeMismatch, message: "Unexpected '\(param.string)' of type '\(try param.getType(context: ctx, infer: constructor.parameter(index: nextParam).type))' in constructor for '\(type.string)'")
             }
             nextParam += par.add
+            while self.values.count < nextParam - 1 {
+                self.values.append(nil)
+            }
+            self.values.append(param) // at pars[nextParam - 1] aka current param
         }
     }
     
     func eval(context: GRPHContext) throws -> GRPHValue {
-        var pars = [GRPHValue?]()
-        var nextParam = 0
-        for param in values {
-            let val = try param.eval(context: context)
-            guard let par = try constructor.parameter(index: nextParam, context: context, exp: param) else {
-                throw GRPHRuntimeError(type: .unexpected, message: "Unknown parameter '\(param.string)' in constructor")
-            }
-            nextParam += par.add
-            while pars.count < nextParam - 1 {
-                pars.append(nil)
-            }
-            pars.append(val) // at pars[nextParam - 1] aka current param
-        }
-        return constructor.executable(context, pars)
+        return constructor.executable(context, try values.map { try $0?.eval(context: context) })
     }
     
     func getType(context: GRPHContext, infer: GRPHType) throws -> GRPHType {
@@ -50,7 +41,7 @@ struct ConstructorExpression: Expression {
     }
     
     var string: String {
-        "\(constructor.type.string)(\(constructor.formattedParameterList(values: values)))"
+        "\(constructor.type.string)(\(constructor.formattedParameterList(values: values.compactMap {$0})))"
     }
     
     var needsBrackets: Bool { false }
